3fda9edea88d19126db9a87afb82668e4bae3b65
gnu_user <gsoc.student@gmail.com>
2013-02-18 13:31:35 -0500

    Added copyright to the source files

--- EightQueens/src/gameboard/GameBoard.java
+++ EightQueens/src/gameboard/GameBoard.java
@@ -1,12 +1,33 @@
 /**
- * Creates a game board of given size which can be adjusted.
- * Some of the important options while using GameBoard are 
- * the ability to keep the board centred.
- * 
- * @author Joseph Heron
+ * Artificial Intelligence Project -- Eight Queens Puzzle
+ *
+ * Copyright (C) 2013, Joseph Heron, Jonathan Gillett, and Daniel Smullen
+ * All rights reserved.
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package gameboard;
 
+
+/**
+ * Creates a game board of given size which can be adjusted. Some of the 
+ * important options while using GameBoard are the ability to keep the 
+ * board centred.
+ * 
+ * @author Joseph Heron
+ */
 public abstract class GameBoard 
 {	
 	protected int column;		//The x size of the grid, the number of columns
--- EightQueens/src/gameboard/GameBoardComponent.java
+++ EightQueens/src/gameboard/GameBoardComponent.java
@@ -1,3 +1,23 @@
+/**
+ * Artificial Intelligence Project -- Eight Queens Puzzle
+ *
+ * Copyright (C) 2013, Joseph Heron, Jonathan Gillett, and Daniel Smullen
+ * All rights reserved.
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package gameboard;
 
 import javax.swing.ImageIcon;
@@ -6,8 +26,7 @@ import java.awt.Color;
 import java.awt.Graphics;			//Used to create a 2D graphic
 import java.awt.Graphics2D;			//Used to Draw a Rectangle and a name
 import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
+
 
 public class GameBoardComponent extends JComponent
 {
