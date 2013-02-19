bbe7f5adcdfbdbc9a4bd7155ba9219c6cbe0f821
gnu_user <gsoc.student@gmail.com>
2013-02-18 23:16:01 -0500

    Added the selection method, which is an iterator

---
+++ EightQueens/src/Selection.java
@@ -0,0 +1,139 @@
+/**
+ * Artificial Intelligence Project -- Eight Queens Puzzle
+ *
+ * Copyright (C) 2013, Jonathan Gillett, Joseph Heron, and Daniel Smullen
+ * All rights reserved.
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Random;
+import java.util.TreeMap;
+
+import com.google.common.collect.Range;
+
+
+/**
+ * Selection method for the chromosomes which uses the roulette wheel
+ * selection method, it takes a fitness distribution of chromosomes and
+ * operates as an iterator to select chromosomes from the population.
+ */
+public class Selection
+{
+	/* Epsilon for ranges defined as the MIN value for Double */
+	private static final Double EPSILON = 1.0E-15;
+	
+	/* The chromosome selection ranges used for roulette wheel */
+	
+	/* TODO NOTE LinkedHashMap MUST be used to maintain the order of items
+	 * (stable) data structure, HashMap does NOT guarantee the order!!!!
+	 */
+	private LinkedHashMap<Chromosome, Range<Double>> selection;
+	
+	/* The left and right bound of the roulette wheel range */
+	private final Double leftBound;
+	private Double rightBound;
+	
+	private final Random random;
+	
+	
+	/** 
+	 * Create an instance of the selection iterator which uses roulette
+	 * wheel selection to select the chromosomes.
+	 * 
+	 * @param random The random number generator to use for the selection.
+	 */
+	public Selection(Random random)
+	{
+		this.random = random;
+		this.selection = new LinkedHashMap<Chromosome, Range<Double>>();
+		
+		/* Initialize the initial left and right bounds */
+		this.leftBound = 0.0;
+		this.rightBound = 0.0 + EPSILON;
+	}
+	
+	
+	/**
+	 * Initialize the selection iterator with the fitness distribution of the 
+	 * chromosomes which is used to generate the selection range of chromosomes
+	 * for the roulette wheel selection method.
+	 * 
+	 * Each range is defined as the left closed and right open, e.g. [0, 3)
+	 * 
+	 * @param fitness The fitness distribution for the chromosomes to be used
+	 * for generating the selection ranges.
+	 */
+	public void init(HashMap<Chromosome, Double> fitness)
+	{
+		/* Lower closed  bound and upper open bound for each interval */
+		Double lower = leftBound;
+		Double upper = leftBound + EPSILON;
+		
+		/* Calculate right bound for the roulette wheel, include the epsilon offset */
+		for (Double value : fitness.values())
+		{
+			rightBound += value + EPSILON;
+		}
+		
+		/* Add each chromosome and interval to roulette wheel selection */
+		for (Chromosome chromosome : fitness.keySet())
+		{
+			upper = lower + fitness.get(chromosome);
+			selection.put(chromosome, Range.closedOpen(lower, upper));
+			
+			/* Increase the lower closed bound to previous right bound + EPSILON */
+			lower = upper + EPSILON;
+		}
+		
+		/* Print each interval
+		for (Chromosome chromosome : selection.keySet())
+		{
+			Range<Double> value = selection.get(chromosome);
+			System.out.println(value.toString());
+		}*/
+	}
+	
+	/**
+	 * The iterator method which uses roulette wheel selection to get the next
+	 * chromosome to be used for the genetic algorithm.
+	 * 
+	 * @return The next chromosome selected using roulette wheel selection
+	 */
+	public Chromosome next() throws ArithmeticException
+	{
+		/* Generate a uniform random number and select a chromosome where the
+		 * random number is within the chromosome's selection interval.
+		 */
+		Double randomNum = random.nextDouble() * rightBound;
+		
+		/* Find the chromosome where the random number is within the interval */
+		for (Chromosome chromosome : selection.keySet())
+		{
+			if (selection.get(chromosome).contains(randomNum))
+			{
+				System.out.println("RIGHT BOUND " + rightBound);
+				System.out.println("VALUE: " + randomNum);
+				System.out.println(selection.get(chromosome).toString());
+				return chromosome;
+			}
+		}
+		
+		/* Arithmetic/rounding error number not found within any interval */
+		throw new ArithmeticException("Arithmetic/Rounding error no chromosome" +
+				" interval found that satisfied the random number");
+	}
+}
