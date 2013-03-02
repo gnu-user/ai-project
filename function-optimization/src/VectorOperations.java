dd421f37e873474206118846bb10ccdc9ef478f2
gnu_user <gsoc.student@gmail.com>
2013-03-02 18:15:52 -0500

    Refactored the method parameter names

--- function-optimization/src/VectorOperations.java
+++ function-optimization/src/VectorOperations.java
@@ -1,17 +1,17 @@
 
 public abstract class VectorOperations {
 
-	private static Vector difference(Vector v1, Vector v2)
+	private static Vector difference(Vector vector1, Vector vector2)
 	{
 		return null;
 	}
 	
-	private static Vector createNoisy(Vector v1, Vector v2)
+	private static Vector createNoisy(Vector vector1, Vector vector2)
 	{
 		return null;
 	}
 	
-	public static Vector mutation(Vector v1, Vector v2, Vector v3)
+	public static Vector mutation(Vector vector1, Vector vector2, Vector vector3)
 	{
 		return null;
 	}
